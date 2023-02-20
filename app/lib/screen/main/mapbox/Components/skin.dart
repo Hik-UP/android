1519ed8f237f566283b3a36a97c77a97a88c8695
imdadadelabou0 <imdadadelabou0@gmail.com>
2023-02-20 20:43:49 +0100

    [feat] Try to change skin of user

--- app/lib/screen/main/mapbox/Components/skin.dart
+++ app/lib/screen/main/mapbox/Components/skin.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
@@ -17,17 +18,33 @@ class _PlayerSkinState extends State<PlayerSkin> {
     AppState _appState = context.read<AppState>();
     print(_appState.skin.pictures);
     return CurrentLocationLayer(
-        followOnLocationUpdate: FollowOnLocationUpdate.always,
-        turnOnHeadingUpdate: TurnOnHeadingUpdate.always,
-        style: const LocationMarkerStyle(
-          marker: DefaultLocationMarker(
-            child: Icon(
-              Icons.navigation,
-              color: Colors.white,
-            ),
+      followOnLocationUpdate: FollowOnLocationUpdate.always,
+      turnOnHeadingUpdate: TurnOnHeadingUpdate.always,
+      style: LocationMarkerStyle(
+        marker: CachedNetworkImage(
+          imageUrl: _appState.skin.pictures[0],
+          errorWidget: (context, url, error) => const Icon(
+            Icons.warning,
+            color: Colors.red,
           ),
-          markerSize: Size(40, 40),
-          markerDirection: MarkerDirection.heading,
-        ));
+        ),
+        markerSize: const Size(40, 40),
+        markerDirection: MarkerDirection.heading,
+      ),
+    );
+  }
+}
+
+class MarkerComponent extends StatelessWidget {
+  const MarkerComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const DefaultLocationMarker(
+      child: Icon(
+        Icons.navigation,
+        color: Colors.white,
+      ),
+    );
   }
 }
